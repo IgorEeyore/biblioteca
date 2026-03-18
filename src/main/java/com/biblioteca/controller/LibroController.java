d5e7bc78a19483fdb68a545c7adaf1ce6e0f290e
sebastian <sebastianytc@gmail.com>
2026-03-18 19:17:11 -0300

    Biblioteca terminada

---
+++ src/main/java/com/biblioteca/controller/LibroController.java
@@ -0,0 +1,63 @@
+package com.biblioteca.controller;
+
+import com.biblioteca.model.Libro;
+import com.biblioteca.service.LibroService;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+// Estos imports son para el crud de la maquina
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+// Estos para los @ antes de la clase
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Estos para los parametros en los metodos del crud
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RestController
+@RequestMapping("/api/v1/libros") // Direccion URL del "localhost" acompañado con el puerto, ej: "localhost:8080/api/v1/libros"
+public class LibroController {
+
+    @Autowired
+    private LibroService libroService;
+
+    @GetMapping
+    public List<Libro> getBooks(){
+        return libroService.getBooks();
+    }
+
+    @GetMapping("{id}") // Cada vez que se pone el id se requiere un @Path como parametro
+    public Libro getBookById(@PathVariable int id){
+        return libroService.getBookById(id);
+    }
+
+    @PostMapping // El @RequestBody exige como parametro un documento JSON en el postman donde se ingresan los datos correspondientes
+    public Libro saveBook(@RequestBody Libro libro){
+        return libroService.saveBook(libro);
+    }
+
+    @PutMapping("{id}") // En este caso se usan ambos porque el metodo es para actualizar un dato, se requiere el @path para escoger la id a editar
+                        // y luego el @request para colocar los datos que van a reemplazar los antiguos
+    public Libro updateBook(@PathVariable int id, @RequestBody Libro libro){
+        return libroService.updateBook(libro);
+    }
+
+    @DeleteMapping("{id}")
+    public String deleteBook(@PathVariable int id){
+        return libroService.deleteBook(id);
+    }
+
+    //@PathVariable para obtener un id exacto
+    //@RequestBody para ingresar un documento JSON, sirve a la hora de ingresar un nuevo registro de datos
+
+    //@GetMapping para acceder a un dato de la lista, se necesita el parametro del id correspondiente que se obtiene con el @PathVariable como parametro
+    //@PostMapping para ingresar datos a la hora de hacer una consulta, se requiere el @RequestBody
+    //@PutMapping para la edicion de un dato, en los parametros de este se requiere el @PathVariable y @RequestBody
+    //@DeleteMapping para la eliminacion de un dato, requiere el @PathVariable
+}
---
+++ src/main/java/com/biblioteca/repository/LibroRepository.java
@@ -0,0 +1,107 @@
+package com.biblioteca.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.biblioteca.model.Libro;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class LibroRepository {
+
+    // Arreglo que va a guardar mis libros
+    private List<Libro> listaLibros = new ArrayList<>();
+
+    // Metodo para inicializar los datos ficticios
+    @PostConstruct
+    public void init(){
+        listaLibros.add(new Libro(1,"978-0134685991","Effective Java","Addison-wesley",2018,"Joshua Bloch"));
+        listaLibros.add(new Libro(2,"978-1617294956","Spring in action","Manning",2020,"Craig Walls"));
+        listaLibros.add(new Libro(3,"978-1491950357","Design Data-Intensive Application","O'Reilly",2017,"Martin Kleppmann"));
+        listaLibros.add(new Libro(4,"978-0132350884","Clean Code","Prentice Hall",2008,"Robert C.Martin"));
+    }
+
+    // Metodo que retorna todos los libros
+    public List<Libro> getAllBooks(){
+        return listaLibros;
+    }
+
+    // Metodo que retorna un libro por su ID
+    public Libro getBookById(int id){
+        for(Libro libro : listaLibros){
+            if (libro.getId() == id) {
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    // Metodo que retorna un libro por su isbn
+    public Libro getBookByIsbn(String isbn){
+        for(Libro libro : listaLibros){
+            if (libro.getIsbn().equals(isbn)) { // Al ser un String no es '==', ahi se usa .equals()
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    // Metodo para guardar un libro
+    public Libro save(Libro lib){
+        listaLibros.add(lib);
+        return lib;
+    }
+
+    // Metodo para actualizar un libro
+    public Libro updateBook(Libro lib){
+        int id = 0;
+        int idPosicion = 0;
+
+        // Primero se busca el libro para tener el id y la posicion
+        for(int i = 0;i<listaLibros.size();i++){
+            if(listaLibros.get(i).getId() == lib.getId()){
+                id = lib.getId();
+                idPosicion = i;
+            }
+        }
+        // Se crea una nueva instancia con el nuevo libro, se mantiene el id y se reemplaza el resto por lo nuevo
+        Libro libro1 = new Libro();
+        libro1.setId(id);
+        libro1.setTitulo(lib.getTitulo());
+        libro1.setAutor(lib.getAutor());
+        libro1.setFechaPublicacion(lib.getFechaPublicacion());
+        libro1.setEditorial(lib.getEditorial());
+        libro1.setIsbn(lib.getIsbn());
+
+        // Se reemplaza en la lista en la misma posicion y se entrega el libro recien actualizado
+        listaLibros.set(idPosicion,libro1);
+        return libro1;
+    }
+
+    // Metodo para eliminar un libro
+    public void deleteBook(int id){
+        
+        // Primera forma de hacer esto
+        Libro libro = getBookById(id);
+        if (libro != null) {
+            listaLibros.remove(libro);
+        }
+
+        // Segunda forma de hacerlo
+        int idPosicion = 0;
+        for(int i = 0;i<listaLibros.size();i++){
+            if(listaLibros.get(i).getId() == id){
+                idPosicion = i;
+                break;
+            }
+        }
+        if(idPosicion > 0){
+            listaLibros.remove(idPosicion);
+        }
+
+        // Tercera forma de hacerlo
+        listaLibros.removeIf(x -> x.getId() == id);
+    }
+}
